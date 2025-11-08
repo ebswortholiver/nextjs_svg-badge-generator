e2bf2218b8bb0b8facb608f78700e77b1916f63a
Oliver Ebsworth <work.ebswortholiver@gmail.com>
2025-11-08 04:17:46 +0000

    refactor: Adjusted the logic for the API route, no longer using puppeteer.

---
+++ app/api/badge/generate/route.ts
@@ -0,0 +1,27 @@
+import { getCustomSVG } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const text = searchParams.get("text") || "TypeScript";
+  const svgName = searchParams.get("svgName") || "typescript";
+  const backgroundColor = searchParams.get("backgroundColor") || "000000";
+  const textColor = searchParams.get("textColor") || "ffffff";
+  const iconColor = searchParams.get("iconColor") || "ffffff";
+
+  const svg = await getCustomSVG({
+    text,
+    svgName,
+    backgroundColor,
+    textColor,
+    iconColor,
+  });
+
+  return new NextResponse(svg, {
+    headers: {
+      "Content-Type": "image/svg+xml",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+}
