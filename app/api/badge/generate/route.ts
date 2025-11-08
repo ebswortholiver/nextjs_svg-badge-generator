34dac06dd7af3b65ab238f0cb27f12b5157619b9
Oliver Ebsworth <work.ebswortholiver@gmail.com>
2025-11-08 16:12:39 +0000

    refactor: Adjusted the default values when no query param is recieved.

--- app/api/badge/generate/route.ts
+++ app/api/badge/generate/route.ts
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
-  const text = searchParams.get("text") || "TypeScript";
+  const text = searchParams.get("text") || "typescript";
   const svgName = searchParams.get("svgName") || "typescript";
-  const backgroundColor = searchParams.get("backgroundColor") || "000000";
+  const backgroundColor = searchParams.get("backgroundColor") || "currentColor";
   const textColor = searchParams.get("textColor") || "ffffff";
   const iconColor = searchParams.get("iconColor") || "ffffff";
 
